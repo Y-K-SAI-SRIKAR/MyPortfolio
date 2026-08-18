7e02ae774b4d7bd84e9a16ba6c0d433f10e2c03a
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-08-18 23:59:26 +0530

    Updated Compoenents in Connect Page

--- src/Pages/Connect.jsx
+++ src/Pages/Connect.jsx
@@ -5,6 +5,9 @@ import Img from '../assets/9.jpg'
 import PixelSnow from "../components/PixelSnow";
 import DitherReveal from "../components/DitherReveal";
 import AnimatedContent from "../components/AnimatedContent";
+import FoldText from "../components/FoldText";
+import BottomUpLetters from "../components/BottumUpLetters";
+import GlassDock from "../components/GlassDock";
 
 
 function Connect(){
@@ -51,8 +54,8 @@ function Connect(){
             <section className="Connect-Content">
                 <AnimatedContent
                         distance={50}
-                        direction="vertical"
-                        reverse={false}
+                        direction="horizontal"
+                        reverse={true}
                         duration={2}
                         ease="power3.out"
                         initialOpacity={0}
@@ -68,8 +71,49 @@ function Connect(){
                     />
                 </div>
                 </AnimatedContent>
+                <div className="Connect-Content-Text">
+                    <div className="Connect-Content-TitleA">
+                        <FoldText
+                            text="Story time is over !"
+                            splitBy="char"
+                            hinge="top"
+                            trigger="scroll"
+                            duration={0.95}
+                            stagger={0.045}
+                            ease="power3.out"
+                            perspective={700}
+                            creaseShading={0.55}
+                            fontSize={70}
+                            fontWeight={650}
+                            color="#e6ebeb"
+                        />
+                    </div>
+                    <div className="Connect-Content-TitleB">
+                        <BottomUpLetters triggerOnView={true}>
+                            Collaboration time? Let's connect.
+                        </BottomUpLetters>
+                    </div>
+                    <div className="Connect-Content-Dock">
+                        <GlassDock
+                            items={[
+                                { icon: "email", title: "Email", href: "" },
+                                { icon: "linkedin", title: "LinkedIn", href: "" },
+                                { icon: "dockerhub", title: "DockerHub", href:"" },
+                                { icon: "instagram", title: "Instagram", href:"" },
+                                { icon: "blog", title: "Blog", href:"" },
+                            ]}
+                        />
+                    </div>
+                </div>
             </section>
         </section>
     )
 }
 export default Connect;
+
+/*<GlassDock
+      items={[
+        { icon: "home", title: "Home", href: "/" },
+        { icon: "settings", title: "Settings", href: "/settings" },
+      ]}
+    />*/
