3d089d79112ea4525eb21a8ba3d4354da0df1439
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-09-13 15:06:11 +0530

    Completed Projects section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,8 +1,42 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Lenis from 'lenis';
 import './Projects.css';
 import Silk from '../components/Silk';
 import FoldText from "../components/FoldText";
+import { CategoryList} from '../components/ProjectAccordion';
+import { Code } from 'lucide-react';
+import DotMatrixText from "../components/DotText";
+
+const sampleCategories = [
+  {
+    id: 1,
+    title: "LossLess Engine",
+    subtitle: "AI Enabled Fraud Detection System",
+    onClick: () => window.open("https://github.com/Y-K-SAI-SRIKAR/LossLess-Engine","_blank"),
+    icon: <Code className="w-8 h-8" />, 
+  },
+  {
+    id: 2,
+    title: "Accident Severity Detection",
+    subtitle: "DL & CV based Severity Inference",
+    onClick: () => window.open("https://github.com/Y-K-SAI-SRIKAR/AccSevDetFrontend","_blank"),
+    icon: <Code className="w-8 h-8" />
+  },
+  {
+    id: 3,
+    title: "WavePoint : Customer Support",
+    subtitle: "React & SpringBoot stacked Application",
+    onClick: () => window.open("https://github.com/Y-K-SAI-SRIKAR/WavePoint-Support","_blank"),
+    icon: <Code className="w-8 h-8" />
+  },
+  {
+    id: 4,
+    title: "Student Performance Analyzer",
+    subtitle: "Data Manipulation Application Built on Numpy-Python",
+    onClick: () => window.open("https://github.com/Y-K-SAI-SRIKAR/StudentPerformenceAnalyzer","_blank"),
+    icon: <Code className="w-8 h-8" />
+  }
+]
 
 function Projects(){
 
@@ -56,10 +90,29 @@ function Projects(){
                     />
                 </div>
                 <div className="Projects-Content-Body">
+                    <div className="Projects-Content-Body-Quote">
+                        <div className="relative z-10 w-full max-w-5xl px-6 h-56 md:h-80 flex items-center justify-center">
+                            <DotMatrixText
+                                text={["Code", "Deploy", "Iterate"]}
+                                transition="fade"
+                                cycleInterval={3000}
+                                dotSize={2.5}
+                                gap={2.5}
+                                activeColor="#ffffff"
+                                inactiveColor="rgba(255, 255, 255, 0.04)"
+                                showInactive={true}
+                                className="w-full h-full drop-shadow-[0_0_25px_rgba(255,255,255,0.4)]"
+                            />
+                        </div>
+                    </div>
+                    <div className="Projects-Content-Body-Acc">
+                        <CategoryList
+                            categories={sampleCategories}
+                        />
+                    </div>
                 </div>
             </section>
         </section>
     )
 }
-
 export default Projects;
