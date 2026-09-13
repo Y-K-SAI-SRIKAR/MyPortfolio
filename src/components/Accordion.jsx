4a0bc40562f7a680dc63f6d735d4046df3863332
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-09-13 13:38:28 +0530

    Updated Content with Recent activities

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -8,23 +8,35 @@ import {
 const items = [
   {
     id: "1",
+    title: "Google Fund My Crazy",
+    content:
+      "Contributed in Google Fund my Crazy initiative. Pitched My vision of Better Roads and Safety in India - Adaptive Speed Governance Network"
+  },
+  {
+    id: "2",
+    title: "RazorPay Buildathon",
+    content:
+      "Participated in Buildathon Conducted by RazorPay and Built LossLess Engine - A Real Time Fraud detection System with Agentic AI &  MLOps support."
+  },
+  {
+    id: "3",
     title: "Startup Innovation Weekend (SIW)",
     content:
       "Selected as a ”Strategic Solver” delegate among ASEAN startups at Startup Innovation Weekend Cambodia 2026,representing NexVitals and contributing to startup innovation and regional healthcare technology discussions."
   },
   {
-    id: "2",
+    id: "4",
     title: "Inceptrix 2.0 Hackathon",
     content:`Secured Top 5 at Inceptrix 2.0, Jain University with IVERAS, leading AI/ML and Cloud development across an emergency chatbot, accident severity classifier, path suggester, and route verifier.`
   },
   {
-    id: "3",
+    id: "5",
     title: "CodeSpark 2025 Hackathon",
     content:
       "Built and integrated IoT and cloud components for a Smart Campus Ecosystem covering AQI monitoring, IoT mesh networking, and an interactive plant dashboard."
   },
   {
-    id: "4",
+    id: "6",
     title: "Core Technical Member",
     content:
       "Implemented A* and RRT path-planning algorithms for obstacle rich autonomous navigation, applying graph traversal, sheuristic search, and complexity-aware design."
@@ -34,7 +46,7 @@ const items = [
 export function Accordion05() {
   return (
     <div className="w-full max-w-3xl mr-auto ml-8 md:ml-24">
-      <Accordion type="single" defaultValue="5" collapsible className="w-full">
+      <Accordion type="single" defaultValue="" collapsible className="w-full">
         {items.map(item => (
           <AccordionItem
             value={item.id}
